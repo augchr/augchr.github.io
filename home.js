38fe03750f64962b6608abf52257642a261201d9
ces08 <christinesong08@gmail.com>
2023-12-24 18:25:57 -0500

    new homepage and profile

---
+++ home.js
@@ -0,0 +1,5 @@
+Array.from(document.getElementsByClassName("navigation")[0]).forEach((val, index)=>{
+    val.style.textDecoration = "none";
+});
+
+document.getElementById("homeLink").style.textDecoration = "underline";
---
+++ portfolio.js
@@ -0,0 +1,5 @@
+Array.from(document.getElementsByClassName("navigation")[0]).forEach((val, index)=>{
+    val.style.textDecoration = "none";
+});
+
+document.getElementById("portfolioLink").style.textDecoration = "underline";
